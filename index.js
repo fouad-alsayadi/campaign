aab3fec120dbf4c8d8385078b680504232359dab
fouad-alsayadi <fouad.alsayadi@databricks.com>
2022-06-05 18:10:50 +0200

    Add google user profile handling with mongoose. note, the follow is not yet finished

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
-require("./services/passport").config();
-require("./routes/authRoutes").config(app);
+// Mongodb connection
+const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@campaign.ohptr.mongodb.net/campaign?retryWrites=true&w=majority`;
+mongoose.connect(mongodbURI,(err)=>{
+    if(err){
+        console.error(err);
+    }else{
+        console.log("Connected to the db susccesfully");
+    }
+});
+
+require('./models/user'); // Register user model in mongoose
+
+// Auth setup
+require("./services/passport").config(); // register Google OAuth 
+require("./routes/authRoutes").config(app); // Handle Google OAuth communications
 
 
 app.get("/", (req, res) => {
@@ -14,3 +28,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
--- services/passport.js
+++ services/passport.js
@@ -1,20 +1,32 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
-exports.config = ()=>{
-    const campaignGoogleStrategy = new GoogleStrategy(
-        {
-          clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-          callbackURL: "/auth/google/callback",
-        },
-        (accessToken, refreshTokem, profile, done) => {
-          console.log("access token", accessToken);
-          console.log("refresh token", refreshTokem);
-          console.log("profile", profile);
+exports.config = () => {
+  const campaignGoogleStrategy = new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshTokem, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshTokem);
+      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          console.log("User ", profile.id, "Exists");
+          done(null, existingUser);
+        } else {
+          const user = new User({
+            googleId: profile.id,
+          });
+          user.save().then((user) => done(null, user));
         }
-      );
-      
-      passport.use(campaignGoogleStrategy);
-}
+      });
+    }
+  );
 
+  passport.use(campaignGoogleStrategy);
+};
